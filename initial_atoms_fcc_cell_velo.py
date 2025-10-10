67536a4be6e293202ec8f5ee00ab3d488703c9a0
Yoshitaka Umeno <104881557+yoshiumeno@users.noreply.github.com>
2025-10-10 21:51:41 +0900

    initial_atoms_fcc_cell_velo.py corrected
    f.close() was added at the end
    some minor changes at the beginning (e.g. order of lines) were made

--- initial_atoms_fcc_cell_velo.py
+++ initial_atoms_fcc_cell_velo.py
@@ -1,6 +1,12 @@
 import math
 import random
 
+lo = 4.04 # Lattice constant of Al
+# how many unit cells?
+irepx = 3
+irepy = 3
+irepz = 3
+
 kb = 1.380649e-23 # Boltzmann constant
 temp = 300.0 # temperature
 wm = 27.0 * 1.67e-27 # Al mass (kg)
@@ -13,8 +19,6 @@ def genv(t,m): # to generate random velocity (ang/s)
     return fac * xx * 1.0e10
 
 f = open('conf_fcc_cell.d','w')
-
-lo = 4.04 # Lattice constant of Al
 
 # Atoms in unit cell
 nu = 4
@@ -34,23 +38,13 @@ rux += [0.0]
 ruy += [lo / 2.0]
 ruz += [lo / 2.0]
 
-# how many unit cells?
-irepx = 3
-irepy = 3
-irepz = 8
-
 # Unit cell size
 lx = lo * irepx
 ly = lo * irepy
 lz = lo * irepz
 
 # prepare array
-rx = []
-ry = []
-rz = []
-vx = []
-vy = []
-vz = []
+rx = []; ry = []; rz = []; vx = []; vy = []; vz = []
 
 icnt = 0
 for ix in range(irepx):
@@ -75,5 +69,5 @@ for i in range(natom):
     print('%d %12.8f %12.8f %12.8f %12.8e %12.8e %12.8e' % (i,rx[i],ry[i],rz[i],vx[i],vy[i],vz[i]))
     print('%12.8f %12.8f %12.8f %12.8e %12.8e %12.8e' % (rx[i],ry[i],rz[i],vx[i],vy[i],vz[i]), file=f)
 
-
+f.close()
 
